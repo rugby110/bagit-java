a5dc22e58042844049b781183807cb2d46018a1d
John Scancella <jsca@loc.gov>
2015-12-04 13:09:00 -0500

    removed commandline runner since it is slow

--- src/main/java/gov/loc/Main.java
+++ src/main/java/gov/loc/Main.java
@@ -1,23 +1,14 @@
 package gov.loc;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import gov.loc.runner.BagitCommandLineRunner;
 
 @SpringBootApplication
 public class Main {
+  protected static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Main.class);
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
-  }
-  
-  @Bean
-  public BagitCommandLineRunner bagitCommandLineRunner(){
-    return new BagitCommandLineRunner();
+    logger.error("RAN FROM MAIN!");
   }
 }
